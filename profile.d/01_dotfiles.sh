9bb3994948a64181464a6ba7388aa9713da2c5f2
Jason Salge <jsalge@cboe.com>
2021-02-19 13:41:00 -0600

    Removing tmac function

--- profile.d/01_dotfiles.sh
+++ profile.d/01_dotfiles.sh
@@ -7,9 +7,3 @@ export PATH=${HOME}/.local/bin:${PATH}
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
 #export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
-function tmac()
-{
-    echo -en "\033]0; $1 \a"
-    tmux -2 attach -t $1 || tmux -2 new-session -s $1
-    echo -en "\033]0; `hostname --short` \a"
-}
